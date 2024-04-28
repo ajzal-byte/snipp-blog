f15954f2595f005bd662f71702266cb543dd8cca
ajzal-byte <ajzalaju10@gmail.com>
2024-04-28 13:20:28 +0530

    contact page ui updated

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from "next/image"
+import styles from "./contact.module.css"
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="contact" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email address" />
+          <input type="text" placeholder="Phone Number (optional)" />
+          <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
