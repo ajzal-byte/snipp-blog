d578e5863d8b7610f6cc0f310c02333514b3d790
ajzal-byte <ajzalaju10@gmail.com>
2024-05-17 04:54:17 +0530

    register functionality added with bcrypt

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { register } from "../../../lib/action";
+import styles from "./register.module.css";
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form className={styles.form} action={register}>
+          <input type="text" placeholder="Username" name="username" />
+          <input type="email" placeholder="Email" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="cpassword"
+          />
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
--- src/lib/action.js
+++ src/lib/action.js
@@ -2,8 +2,9 @@
 
 import { revalidatePath } from "next/cache";
 import { connectToDB } from "./connectToDB";
-import { Post } from "./models";
+import { Post, User } from "./models";
 import { signIn, signOut } from "./auth";
+import bcrypt from "bcrypt";
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -48,4 +49,49 @@ export const handleGithubLogin = async () => {
 };
 export const handleLogout = async () => {
   await signOut();
+};
+
+export const register = async (formData) => {
+  const { username, email, password, cpassword } = Object.fromEntries(formData);
+  if (password !== cpassword) return "Passwords do not match";
+
+  try {
+    connectToDB();
+
+    const user = await User.findOne({ email });
+    if (user) return "User already exists";
+
+    const thisUsername = await User.findOne({ username });
+    if (thisUsername) return "This username is already taken";
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    console.log("Saved to db");
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
+};
+
+export const login = async (formData) => {
+  const { username, password } = Object.fromEntries(formData);
+
+  try {
+    await signIn("credentials", { username, password });
+    // connectToDB();
+
+    // const user = await User.findOne({ username });
+
+    // if (!user) return "User doesn't exist";
+  } catch (error) {
+    console.error(error);
+    throw new Error(error);
+  }
 };
