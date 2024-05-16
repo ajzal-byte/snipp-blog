7e9172066c2bf90dcbc65c8dd986ca1db981b68b
ajzal-byte <ajzalaju10@gmail.com>
2024-05-17 03:25:03 +0530

    authjs signin user added to db

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { connectToDB } from "./connectToDB";
+import { User } from "./models";
 export const {
   handlers: { GET, POST },
   auth,
@@ -19,4 +21,32 @@ export const {
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      if (account.provider === "github") {
+        connectToDB();
+        try {
+          console.log("email", profile.email);
+          const user = await User.findOne({ email: profile.email });
+
+          console.log(user);
+
+          if (!user) {
+            const newUser = new User({
+              username: profile.login,
+              email: profile.email,
+              profilePic: profile.avatar_url,
+            });
+
+            await newUser.save();
+            console.log("New User: ", newUser);
+          }
+        } catch (error) {
+          console.error(error);
+          return false;
+        }
+      }
+      return true;
+    },
+  },
 });
