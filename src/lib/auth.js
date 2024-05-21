25082d0e44e4a29252a6b3c3a15431a09965ca9a
ajzal-byte <ajzalaju10@gmail.com>
2024-05-21 10:42:20 +0530

    middleware added to protect routes

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,6 +4,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDB } from "./connectToDB";
 import { User } from "./models";
 import bcrypt from "bcrypt";
+import { authConfig } from "./auth.config";
 
 const login = async (credentials) => {
   try {
@@ -16,7 +17,6 @@ const login = async (credentials) => {
       credentials.password,
       user.password
     );
-
 
     if (!isPasswordCorrect) throw new Error("Incorrect Password");
 
@@ -31,6 +31,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  ...authConfig,
   providers: [
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -81,5 +82,6 @@ export const {
       }
       return true;
     },
+    ...authConfig.callbacks
   },
 });
